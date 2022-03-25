0132fd7fe8c502d596542c4084c67441020e3a72
wuqunqun <wuqunqun@qq.com>
2022-03-25 15:46:33 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+let http = require('http')
+let users = [
+  {id:1,name:'qunqun1'},
+  {id:2,name:'qunqun2'},
+  {id:3,name:'qunqun3'},
+]
+let server = http.createServer(function(req,res){
+  res.setHeader('Access-Control-Allow-Origin','*')
+  if(req.url === '/api/users'){
+    res.end(JSON.stringify(users))
+  }else{
+    res.end('Not Fount')
+  }
+})
+
+server.listen(3000,()=>{
+  console.log('后端API已经启动在3000端口了！')
+})
